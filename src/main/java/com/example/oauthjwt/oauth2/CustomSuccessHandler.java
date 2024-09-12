847ccae56b57f5d25fd2906e00397b36b7f155d9
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-12 16:43:53 +0900

    OAuth2 로그인 성공 핸들러 작성

---
+++ src/main/java/com/example/oauthjwt/oauth2/CustomSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.example.oauthjwt.oauth2;
+
+import com.example.oauthjwt.dto.CustomOAuth2User;
+import com.example.oauthjwt.jwt.JWTUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@Component
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
+
+    public CustomSuccessHandler(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+}
