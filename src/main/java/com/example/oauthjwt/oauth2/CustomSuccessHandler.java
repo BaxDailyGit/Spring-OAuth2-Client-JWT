95dc2b29ddfe7671ba7ac41daaa2b064a7902205
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-12 16:44:30 +0900

    jwt 응답 메소드 구현

--- src/main/java/com/example/oauthjwt/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/example/oauthjwt/oauth2/CustomSuccessHandler.java
@@ -21,4 +21,24 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         this.jwtUtil = jwtUtil;
     }
 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        //OAuth2User
+        CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
+
+        String username = customUserDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(username, role, 60*60*60L);
+
+        response.addCookie(createCookie("Authorization", token));
+        response.sendRedirect("http://localhost:3000/");
+    }
+
+    
 }
