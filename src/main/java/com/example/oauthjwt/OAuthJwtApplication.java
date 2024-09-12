d57fc83c249eacc1ca7eece465fd7c81d35e23f5
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-12 13:32:40 +0900

    init

---
+++ src/main/java/com/example/oauthjwt/OAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthJwtApplication.class, args);
+    }
+
+}
