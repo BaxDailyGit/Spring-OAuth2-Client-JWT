3933aa30f77e70bc06385bed74f17cafe4afeb1b
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-12 16:48:04 +0900

    SecurityConfig 핸들러 등록

--- src/main/java/com/example/oauthjwt/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/example/oauthjwt/oauth2/CustomSuccessHandler.java
@@ -2,13 +2,16 @@ package com.example.oauthjwt.oauth2;
 
 import com.example.oauthjwt.dto.CustomOAuth2User;
 import com.example.oauthjwt.jwt.JWTUtil;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
