38e81898a3e7749137222127a9db68e92b54ba6a
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-12 14:27:17 +0900

    컨트롤러 생성

---
+++ src/main/java/com/example/oauthjwt/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.oauthjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my")
+    @ResponseBody
+    public String myAPI() {
+
+        return "my route";
+    }
+}
